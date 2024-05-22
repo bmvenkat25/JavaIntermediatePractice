edafe5f2760b43cc4a28ede7c7f33e34e4635de9
bmvenkat25 <bmvenkat25@gmail.com>
2024-05-22 08:57:06 +0530

    Polymorphism

--- src/main/java/Polymorphism/RunTimePolymorphism.java
+++ src/main/java/Polymorphism/RunTimePolymorphism.java
@@ -6,12 +6,25 @@ class Parent {
         System.out.println("Method from Parent class called");
     }
 }
-public class RunTimePolymorphism extends Parent{
+
+class Child extends Parent {
     public void methodOverRide() {
         System.out.println("Method from Child class called");
     }
+}
+public class RunTimePolymorphism extends Child{
+    public void methodOverRide() {
+        System.out.println("Method from RunTimePolymorphism class called");
+    }
     public static void main(String[] args){
-        new RunTimePolymorphism().methodOverRide();
+        Parent p = new Parent();
+        p.methodOverRide();
+
+        p = new Child();
+        p.methodOverRide();
+
+        p = new RunTimePolymorphism();
+        p.methodOverRide();
     }
 }
 
